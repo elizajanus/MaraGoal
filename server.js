97c7a3cf49b5a7849cfbd07cc494ac311f004889
Eliza Janus <emj4em@virginia.edu>
2018-07-14 15:01:30 -0400

    build stuff.

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
- app.use(express.static("client/public"));
+ app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
