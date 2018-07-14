712e73844e5ed2268cb65e09f9cd1d664190c5ef
Karen Brant <karen.brant3@gmail.com>
2018-07-14 10:51:36 -0400

    made changes to App.js & got rid of header in diary

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,14 +4,17 @@ import NavTabs from "./NavTabs";
 import ChatApp from './chat/ChatApp';
 import CalendarApp from './calendar/CalendarApp';
 import DiaryApp from "./diary/DiaryApp";
-import SignupApp from './SignupApp';
+import SignupApp from './SignupApp/SignupApp';
+import LoginApp from './LoginApp/LoginApp';
 
 const App = () =>  (
     <Router>
         <div>
-            <SignupApp />
             <NavTabs />
             <Switch>
+                <Route exact path="/" component={SignupApp} />
+                <Route exact path="/login" component={LoginApp} />
+                <Route exact path="/signup" component={SignupApp} />
                 <Route exact path="/calendar" component={CalendarApp} />
                 <Route exact path="/groupchat" component={ChatApp} />
                 <Route exact path="/diary" component={DiaryApp} />
