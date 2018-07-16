2337289d2ae87a1d62e52f46521b9dffe6aecbed
Katherine Eaves <32962110+koeaves@users.noreply.github.com>
2018-07-16 19:47:21 -0400

    change fonts and opacity for diary form

--- client/src/components/diary/diary.js
+++ client/src/components/diary/diary.js
@@ -8,6 +8,9 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import "./diary.css";
+
 
 class Diary extends React.Component {
   state = {
@@ -95,90 +98,94 @@ class Diary extends React.Component {
     return (
       <div>
        <AppBar position="static">
-        <Toolbar>
+        <Toolbar class= "running-form-toolbar" color= "blue">
           <Typography variant="title" color="inherit">
             How Was Your Run Today?
           </Typography>
         </Toolbar>
       </AppBar>
-          <form backgroundColor= "white">
-            <TextField
-              name="dateOfRun"
-              hintText="ex 10/10/2018"
-              floatingLabelText="Date of Run"
-              value={this.state.dateOfRun}
-              onChange={e => this.change(e)}
-              floatingLabelFixed
-            />
-            <br />
-            <TextField
-              name="runningTime"
-              hintText="ex 9:30"
-              floatingLabelText="Average Pace (mm:ss)"
-              value={this.state.runningTime}
-              onChange={e => this.change(e)}
-              floatingLabelFixed
-            />
-            <br />
-            <TextField
-              name="runningDistance"
-              hintText="ex 14 mi"
-              floatingLabelText="Running Distance (mi or km)"
-              value={this.state.runningDistance}
-              onChange={e => this.change(e)}
-              floatingLabelFixed
-            />
-            <br />
-            <TextField
-              name="runningSurface"
-              hintText="ex Road, Track, Trail, Beach, etc..."
-              floatingLabelText="Running Surface "
-              value={this.state.runningSurface}
-              onChange={e => this.change(e)}
-              floatingLabelFixed
-            />
-            <br />
-            <TextField
-              name="runningInjury"
-              hintText="ex None, Knee, Ankle, Neck, Back, "
-              floatingLabelText="Current Injuries"
-              value={this.state.runningInjury}
-              onChange={e => this.change(e)}
-              floatingLabelFixed
-            />
-            <br />
-            <TextField
-              name="weatherOnRun"
-              hintText="ex Sunny, Humid, Chilly, Rainy"
-              floatingLabelText="Weather on Run"
-              value={this.state.weatherOnRun}
-              onChange={e => this.change(e)}
-              floatingLabelFixed
-            />
-            <br />
-            <TextField
-              name="soloOrGroup"
-              hintText="ex Solo"
-              floatingLabelText="Solo or Group Run?"
-              value={this.state.soloOrGroup}
-              onChange={e => this.change(e)}
-              floatingLabelFixed
-            />
-            <br />
-            <TextField
-              name="speedHillsOrNormal"
-              hintText="ex Speed work?"
-              floatingLabelText="Speed, Hill, or Easy?"
-              value={this.state.speedHillsOrNormal}
-              onChange={e => this.change(e)}
-              floatingLabelFixed
-            />
-            <br/>
-            <RaisedButton label="Submit" color="inherit" onClick={this.onSubmit} primary />
-          </form>
+      <form>
+        <TextField class= "running-form"
+          name="dateOfRun"
+          hintText="ex 10/10/2018"
+          floatingLabelText="Date of Run"
+          value={this.state.dateOfRun}
+          onChange={e => this.change(e)}
+          floatingLabelFixed
+        
+        />
+        <br />
+        <TextField class= "running-form"
+          name="runningTime"
+          hintText="ex 9:30"
+          floatingLabelText="Average Pace (mm:ss)"
+          value={this.state.runningTime}
+          onChange={e => this.change(e)}
+          floatingLabelFixed
+        />
+        <br />
+        <TextField class= "running-form"
+          name="runningDistance"
+          hintText="ex 14 mi"
+          floatingLabelText="Running Distance (mi or km)"
+          value={this.state.runningDistance}
+          onChange={e => this.change(e)}
+          floatingLabelFixed
+        />
+        <br />
+        <TextField class= "running-form"
+          name="runningSurface"
+          hintText="ex Road, Track, Trail, Beach, etc..."
+          floatingLabelText="Running Surface "
+          value={this.state.runningSurface}
+          onChange={e => this.change(e)}
+          floatingLabelFixed
+        />
+        <br />
+        <TextField class= "running-form"
+          name="runningInjury"
+          hintText="ex None, Knee, Ankle, Neck, Back, "
+          floatingLabelText="Current Injuries"
+          value={this.state.runningInjury}
+          onChange={e => this.change(e)}
+          floatingLabelFixed
+        />
+        <br />
+        <TextField class= "running-form"
+          name="weatherOnRun"
+          hintText="ex Sunny, Humid, Chilly, Rainy"
+          floatingLabelText="Weather on Run"
+          value={this.state.weatherOnRun}
+          onChange={e => this.change(e)}
+          floatingLabelFixed
+        />
+        <br />
+        <TextField class= "running-form"
+          name="soloOrGroup"
+          hintText="ex Solo"
+          floatingLabelText="Solo or Group Run?"
+          value={this.state.soloOrGroup}
+          onChange={e => this.change(e)}
+          floatingLabelFixed
+        />
+        <br />
+        <TextField class= "running-form"
+          name="speedHillsOrNormal"
+          hintText="ex Speed work?"
+          floatingLabelText="Speed, Hill, or Easy?"
+          value={this.state.speedHillsOrNormal}
+          onChange={e => this.change(e)}
+          floatingLabelFixed
+        />
+        <br/>
+        <RaisedButton label="Submit" color="inherit" onClick={this.onSubmit} primary />
+      </form>
       </div>
     );
   };
 }
 
 export default Diary;
+
+
+    
