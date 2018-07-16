4a29fcde4c0cf15c1533dafb210ee57a23886669
Katherine Eaves <32962110+koeaves@users.noreply.github.com>
2018-07-16 12:19:25 -0400

    add appBar to create visual breaks in diaryApp

--- client/src/components/diary/Table.js
+++ client/src/components/diary/Table.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import data from './diaryEntries.json';
 import diary from "./diary.js";
-import axios from 'axios';
-import LoginApp from "../LoginApp/LoginApp.js";
-import API from '../../utils/API';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
  
 import {
   Table,
@@ -15,70 +17,55 @@ import {
 } from "material-ui/Table";
 
 class DiaryTable extends React.Component {
-  
   state = {
-    diaryEntries: []
+    diaryEntries: [],
+    username: ""
   }
-  
-  componentDidMount() {
-    const that = this;
-    API.getRunStats()
-      .then(function(res) {
-        console.log(res.data);
-        const diaryEntries = res.data;
-        console.log(that);
-        that.setState({ diaryEntries: res.data });
-      })
+  propTypes = {
+    diaryEntries: PropTypes.array
   }
-  // getDiaryEntries = (e) => {
-    
-  //   e.preventDefault();
-  //   axios.get("/api/diary", {
-  //       params: sessionStorage["username"]
-  //   }).then(function(response) {
-  //       console.log("this is my table:" + response);
-  //   }).catch(function(err) {
-  //       console.log(err);
-  //   });
-  // }
 
-  
-    // get username or unique ID from session storage
-    // Make AJAX request to  server to load diary entries belonging to that user
-    // In the request's "then" handler, update the state's diaryEntries property with the data returned
   render() {
-    
     return (
-      <Table>
-      <TableHeader>
-        <TableRow>
-            <TableHeaderColumn>Date</TableHeaderColumn>
-            <TableHeaderColumn>Pace</TableHeaderColumn>
-            <TableHeaderColumn>Distance</TableHeaderColumn>
-            <TableHeaderColumn>Surface</TableHeaderColumn>
-            <TableHeaderColumn>Injury</TableHeaderColumn>
-            <TableHeaderColumn>Weather</TableHeaderColumn>
-            <TableHeaderColumn>Solo/Group</TableHeaderColumn>
-            <TableHeaderColumn>Speed/Hill/Normal</TableHeaderColumn>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {this.state.diaryEntries.map(row => {
-          return (
-            <TableRow>
-              <TableRowColumn>{row.dateOfRun}</TableRowColumn>
-              <TableRowColumn>{row.runningTime}</TableRowColumn>
-              <TableRowColumn>{row.runningDistance}</TableRowColumn>
-              <TableRowColumn>{row.runningSurface}</TableRowColumn>
-              <TableRowColumn>{row.runningInjury}</TableRowColumn>
-              <TableRowColumn>{row.weatherOnRun}</TableRowColumn>
-              <TableRowColumn>{row.soloOrGroup}</TableRowColumn>
-              <TableRowColumn>{row.speedHillsOrNormal}</TableRowColumn>
-            </TableRow>
-           ); 
-          })}
-      </TableBody>
-    </Table>
+      <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            Your Running Diary
+          </Typography>
+        </Toolbar>
+      </AppBar>
+        <Table>
+        <TableHeader>
+          <TableRow>
+              <TableHeaderColumn>Date</TableHeaderColumn>  
+              <TableHeaderColumn>Pace</TableHeaderColumn>
+              <TableHeaderColumn>Distance</TableHeaderColumn>
+              <TableHeaderColumn>Surface</TableHeaderColumn>
+              <TableHeaderColumn>Injury</TableHeaderColumn>
+              <TableHeaderColumn>Weather</TableHeaderColumn>
+              <TableHeaderColumn>Solo/Group</TableHeaderColumn>
+              <TableHeaderColumn>Speed/Hill/Normal</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {
+            this.props.diaryEntries.map(row => (
+              <TableRow>
+                <TableRowColumn>{row.dateOfRun}</TableRowColumn>
+                <TableRowColumn>{row.runningTime}</TableRowColumn>
+                <TableRowColumn>{row.runningDistance}</TableRowColumn>
+                <TableRowColumn>{row.runningSurface}</TableRowColumn>
+                <TableRowColumn>{row.runningInjury}</TableRowColumn>
+                <TableRowColumn>{row.weatherOnRun}</TableRowColumn>
+                <TableRowColumn>{row.soloOrGroup}</TableRowColumn>
+                <TableRowColumn>{row.speedHillsOrNormal}</TableRowColumn>
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </Table>
+    </div>
     );
   }
 }
--- client/src/components/diary/diary.js
+++ client/src/components/diary/diary.js
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import TextField from "material-ui/TextField";
 import RaisedButton from "material-ui/RaisedButton";
 import axios from 'axios';
-// import LoginApp from "../LoginApp"
+import LoginApp from "../LoginApp";
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class Diary extends React.Component {
   state = {
@@ -25,6 +30,10 @@ class Diary extends React.Component {
     speedHillsOrNormalError: ""
   };
 
+  propTypes = {
+    onAddEntry: PropTypes.func
+  }
+
   change = e => {
     this.setState({
       [e.target.name]: e.target.value
@@ -46,7 +55,7 @@ class Diary extends React.Component {
 
     if (this.state.length < 1) {
       isError = true;
-      errors.dateOfRun = "This is a required field";
+      errors.usernameError = "This is a required field";
     }
 
     this.setState({
@@ -60,7 +69,7 @@ class Diary extends React.Component {
   onSubmit = e => {
     e.preventDefault();
    const entry = {
-    username: sessionStorage["username"],
+    username: this.state.username,
     dateOfRun: this.state.dateOfRun,
     runningTime: this.state.runningTime,
     runningDistance: this.state.runningDistance,
@@ -75,6 +84,8 @@ class Diary extends React.Component {
    .then(res => {
      console.log(res);
      console.log(res.data);
+      this.props.onAddEntry(res.data);
+     console.log(this.props);
    })
    .catch(err => console.log(err));
 
@@ -82,81 +93,90 @@ class Diary extends React.Component {
 
   render() {
     return (
-      <form>
-        <TextField
-          name="dateOfRun"
-          hintText="ex 10/10/2018"
-          floatingLabelText="Date of Run"
-          value={this.state.dateOfRun}
-          onChange={e => this.change(e)}
-          floatingLabelFixed
-        />
-        <br />
-        <TextField
-          name="runningTime"
-          hintText="ex 9:30"
-          floatingLabelText="Average Pace (mm:ss)"
-          value={this.state.runningTime}
-          onChange={e => this.change(e)}
-          floatingLabelFixed
-        />
-        <br />
-        <TextField
-          name="runningDistance"
-          hintText="ex 14 mi"
-          floatingLabelText="Running Distance (mi or km)"
-          value={this.state.runningDistance}
-          onChange={e => this.change(e)}
-          floatingLabelFixed
-        />
-        <br />
-        <TextField
-          name="runningSurface"
-          hintText="ex Road, Track, Trail, Beach, etc..."
-          floatingLabelText="Running Surface "
-          value={this.state.runningSurface}
-          onChange={e => this.change(e)}
-          floatingLabelFixed
-        />
-        <br />
-        <TextField
-          name="runningInjury"
-          hintText="ex None, Knee, Ankle, Neck, Back, "
-          floatingLabelText="Current Injuries"
-          value={this.state.runningInjury}
-          onChange={e => this.change(e)}
-          floatingLabelFixed
-        />
-        <br />
-        <TextField
-          name="weatherOnRun"
-          hintText="ex Sunny, Humid, Chilly, Rainy"
-          floatingLabelText="Weather on Run"
-          value={this.state.weatherOnRun}
-          onChange={e => this.change(e)}
-          floatingLabelFixed
-        />
-        <br />
-        <TextField
-          name="soloOrGroup"
-          hintText="ex Solo"
-          floatingLabelText="Solo or Group Run?"
-          value={this.state.soloOrGroup}
-          onChange={e => this.change(e)}
-          floatingLabelFixed
-        />
-        <br />
-        <TextField
-          name="speedHillsOrNormal"
-          hintText="ex Speed work?"
-          floatingLabelText="Speed, Hill, or Easy?"
-          value={this.state.speedHillsOrNormal}
-          onChange={e => this.change(e)}
-          floatingLabelFixed
-        />
-        <br/>
-        <RaisedButton label="Submit" onClick={this.onSubmit} primary />
-      </form>
+      <div>
+       <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            How Was Your Run Today?
+          </Typography>
+        </Toolbar>
+      </AppBar>
+          <form backgroundColor= "white">
+            <TextField
+              name="dateOfRun"
+              hintText="ex 10/10/2018"
+              floatingLabelText="Date of Run"
+              value={this.state.dateOfRun}
+              onChange={e => this.change(e)}
+              floatingLabelFixed
+            />
+            <br />
+            <TextField
+              name="runningTime"
+              hintText="ex 9:30"
+              floatingLabelText="Average Pace (mm:ss)"
+              value={this.state.runningTime}
+              onChange={e => this.change(e)}
+              floatingLabelFixed
+            />
+            <br />
+            <TextField
+              name="runningDistance"
+              hintText="ex 14 mi"
+              floatingLabelText="Running Distance (mi or km)"
+              value={this.state.runningDistance}
+              onChange={e => this.change(e)}
+              floatingLabelFixed
+            />
+            <br />
+            <TextField
+              name="runningSurface"
+              hintText="ex Road, Track, Trail, Beach, etc..."
+              floatingLabelText="Running Surface "
+              value={this.state.runningSurface}
+              onChange={e => this.change(e)}
+              floatingLabelFixed
+            />
+            <br />
+            <TextField
+              name="runningInjury"
+              hintText="ex None, Knee, Ankle, Neck, Back, "
+              floatingLabelText="Current Injuries"
+              value={this.state.runningInjury}
+              onChange={e => this.change(e)}
+              floatingLabelFixed
+            />
+            <br />
+            <TextField
+              name="weatherOnRun"
+              hintText="ex Sunny, Humid, Chilly, Rainy"
+              floatingLabelText="Weather on Run"
+              value={this.state.weatherOnRun}
+              onChange={e => this.change(e)}
+              floatingLabelFixed
+            />
+            <br />
+            <TextField
+              name="soloOrGroup"
+              hintText="ex Solo"
+              floatingLabelText="Solo or Group Run?"
+              value={this.state.soloOrGroup}
+              onChange={e => this.change(e)}
+              floatingLabelFixed
+            />
+            <br />
+            <TextField
+              name="speedHillsOrNormal"
+              hintText="ex Speed work?"
+              floatingLabelText="Speed, Hill, or Easy?"
+              value={this.state.speedHillsOrNormal}
+              onChange={e => this.change(e)}
+              floatingLabelFixed
+            />
+            <br/>
+            <RaisedButton label="Submit" color="inherit" onClick={this.onSubmit} primary />
+          </form>
+      </div>
     );
   };
 }
